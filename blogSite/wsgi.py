443ae001511e64673b80accbd6afda80b1f6ab73
chetanbachadi <chetanbachadi@gmail.com>
2026-06-22 19:58:40 +0530

    Update wsgi.py

--- blogSite/wsgi.py
+++ blogSite/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = "/home/chetanbachadi2003/My-View"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "blogSite.settings"
+)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogSite.settings')
-
 application = get_wsgi_application()
